23999721b271bff3ef34e271c26b5f723dad67be
Yusuf <houari.ymp@gmail.com>
2022-12-14 13:32:42 +0100

    edit the lochost

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-!&%$2y)pz8m%vkobvnikj(_2fv^ork15ojd7s+22dxkl$@377j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
